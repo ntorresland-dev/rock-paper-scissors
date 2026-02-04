4cf1f8d39250b7096ce082377794e053059cffc2
Nicolas <245983517+ntorresland-dev@users.noreply.github.com>
2026-02-04 17:36:13 -0300

    feat: complete playRound logic with score tracking

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+
+
 function getComputerChoice(){
     let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -17,4 +27,24 @@ function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper, or Scissors?\nType your choice:");
     
     return humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie! Both chose " + humanChoice);
+        return;
+    }
+
+    if (
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    }else {
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
 }
